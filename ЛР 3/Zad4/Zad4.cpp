2f85ec25752c119ec293ccba98c0296afd0468aa
TeflonDervish <voltvilty45@gmail.com>
2023-11-23 10:10:25 +0500

    Just commit

---
+++ ЛР 3/Zad4/Zad4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <bitset>
+
+void encryptDecryptFile(const std::string& inputFile, const std::string& outputFile, const std::string& key) {
+    std::ifstream input(inputFile, std::ios::binary);
+    std::ofstream output(outputFile, std::ios::binary);
+
+    if (!input.is_open() || !output.is_open()) {
+        std::cerr << "Error opening files!" << std::endl;
+        return;
+    }
+
+    // Convert key to binary representation
+    std::bitset<8> binaryKey(key[0]);
+
+    char ch;
+    while (input.get(ch)) {
+        // XOR each byte with the key
+        ch ^= binaryKey.to_ulong();
+
+        // Write the result to the output file
+        output.put(ch);
+
+        // Rotate key for the next byte
+        binaryKey = (binaryKey << 1) | (binaryKey >> 7);
+    }
+
+    input.close();
+    output.close();
+}
+
+int main() {
+    // Пример использования
+    std::string inputFile = "input.txt";
+    std::string encryptedFile = "encrypted.txt";
+    std::string decryptedFile = "decrypted.txt";
+    std::string key = "K";  // Ключ для шифрования (может быть любым символом)
+
+    // Зашифровать файл
+    encryptDecryptFile(inputFile, encryptedFile, key);
+    std::cout << "File encrypted successfully." << std::endl;
+
+    // Расшифровать файл
+    encryptDecryptFile(encryptedFile, decryptedFile, key);
+    std::cout << "File decrypted successfully." << std::endl;
+
+    return 0;
+}
+1   
---
+++ ЛР 3/lr3.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+def generate_playfair_key(key):
+    key = key.replace("J", "I")  # Заменяем J на I
+    key = key.upper()  # Приводим к верхнему регистру
+    key = "".join(sorted(set(key), key=key.find))  # Удаляем повторяющиеся символы и сортируем по порядку ключа
+
+    # Создаем матрицу 5x5 и заполняем ее символами из ключа, затем добавляем оставшиеся буквы алфавита
+    matrix = [['' for _ in range(5)] for _ in range(5)]
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+
+    key_index = 0
+    for i in range(5):
+        for j in range(5):
+            if key_index < len(key):
+                matrix[i][j] = key[key_index]
+                key_index += 1
+            else:
+                while alphabet[0] in key:
+                    alphabet = alphabet[1:]
+                matrix[i][j] = alphabet[0]
+                alphabet = alphabet[1:]
+
+    return matrix
+
+def find_position(matrix, letter):
+    # Функция для нахождения позиции буквы в матрице
+    for i, row in enumerate(matrix):
+        if letter in row:
+            return i, row.index(letter)
+
+def encrypt_playfair(plain_text, key):
+    # Функция для шифрования текста шифром Плейфера
+    playfair_matrix = generate_playfair_key(key)
+    cipher_text = ""
+
+    for i in range(0, len(plain_text), 2):
+        pair = plain_text[i:i + 2]
+
+        if len(pair) == 1:
+            pair += 'X'
+
+        row1, col1 = find_position(playfair_matrix, pair[0])
+        row2, col2 = find_position(playfair_matrix, pair[1])
+
+        if row1 == row2:
+            cipher_text += playfair_matrix[row1][(col1 + 1) % 5] + playfair_matrix[row2][(col2 + 1) % 5]
+        elif col1 == col2:
+            cipher_text += playfair_matrix[(row1 + 1) % 5][col1] + playfair_matrix[(row2 + 1) % 5][col2]
+        else:
+            cipher_text += playfair_matrix[row1][col2] + playfair_matrix[row2][col1]
+
+    return cipher_text
+
+def decrypt_playfair(cipher_text, key):
+    # Функция для дешифрования текста шифром Плейфера
+    playfair_matrix = generate_playfair_key(key)
+    plain_text = ""
+
+    for i in range(0, len(cipher_text), 2):
+        pair = cipher_text[i:i + 2]
+
+        row1, col1 = find_position(playfair_matrix, pair[0])
+        row2, col2 = find_position(playfair_matrix, pair[1])
+
+        if row1 == row2:
+            plain_text += playfair_matrix[row1][(col1 - 1) % 5] + playfair_matrix[row2][(col2 - 1) % 5]
+        elif col1 == col2:
+            plain_text += playfair_matrix[(row1 - 1) % 5][col1] + playfair_matrix[(row2 - 1) % 5][col2]
+        else:
+            plain_text += playfair_matrix[row1][col2] + playfair_matrix[row2][col1]
+
+    return plain_text
+
+# Пример использования
+key = "DEVELOPMENT"
+plain_text = "SEMENOV"
+cipher_text = encrypt_playfair(plain_text, key)
+decrypted_text = decrypt_playfair(cipher_text, key)
+
+print(f"Original Text: {plain_text}")
+print(f"Encrypted Text: {cipher_text}")
+print(f"Decrypted Text: {decrypted_text}")
